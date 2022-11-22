a9eb5556e22b35cc75c495652c7d1dd0ce874273
gabolano17 <gabolano60@misena.edu.co>
2022-11-21 19:11:09 -0500

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import Chart from 'chart.js';
+
+// core components
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2
+} from "./variables/charts";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pmng';
+export class AppComponent implements OnInit {
+
+  public viewModal: boolean = true;
+  public styles = {
+    width: '50vw'
+  }
+
+  public datasets: any;
+  public data: any;
+  public salesChart:any;
+  public clicked: boolean = true;
+  public clicked1: boolean = false;
+
+  ngOnInit() {
+
+    this.datasets = [
+      [0, 20, 10, 30, 15, 40, 20, 60, 60],
+      [0, 20, 5, 25, 10, 30, 15, 40, 40]
+    ];
+    this.data = this.datasets[0];
+
+
+    const chartOrders = document.getElementById('chart-orders');
+
+    parseOptions(Chart, chartOptions());
+
+
+    const ordersChart = new Chart(chartOrders, {
+      type: 'bar',
+      options: chartExample2.options,
+      data: chartExample2.data
+    });
+
+    const chartSales = document.getElementById('chart-sales');
+
+    this.salesChart = new Chart(chartSales, {
+			type: 'line',
+			options: chartExample1.options,
+			data: chartExample1.data
+		});
+  }
+
+
+  public updateOptions() {
+    this.salesChart.data.datasets[0].data = this.data;
+    this.salesChart.update();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {TabViewModule} from 'primeng/tabview';
+import {DialogModule} from 'primeng/dialog'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabViewModule,
+    DialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
